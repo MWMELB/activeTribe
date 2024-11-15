601fd89e431dd3d32581d040bd24a5744b4b7420
Ivy Ta <131229797+ivy-ta@users.noreply.github.com>
2024-11-15 21:04:58 +1100

    activity-controller SHOW view, without geocoder

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,14 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: %i[ show ]
   def index
     @activities = Activity.all
   end
 
+  def show
+  end
+
   private
   def set_activity
-
+    @activity = Activity.find(params[:id])
   end
 end
