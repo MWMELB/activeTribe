1193a3a40c2652abfe2288194ca5ca105eb5701d
RUI KAN <ruikan0809@gmail.com>
2024-11-16 16:14:05 +1300

    fix create a new activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_15_100432) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_16_013136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_15_100432) do
     t.string "sport"
     t.float "longitude"
     t.float "latitude"
+    t.string "category"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
