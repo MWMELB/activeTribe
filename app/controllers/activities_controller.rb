e2be817ca71b8dd71e2235b5a41b8a019aa7f3dc
RUI KAN <ruikan0809@gmail.com>
2024-11-16 14:06:13 +1300

    signup page and activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-  before_action :set_activity, only: %i[ show ]
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
   def index
     @activities = Activity.all
   end
@@ -7,8 +7,47 @@ class ActivitiesController < ApplicationController
   def show
   end
 
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      flash[:notice] = "Activity created successfully!"
+      redirect_to @activity
+    else
+      flash.now[:alert] = "There was an error creating the activity."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to @activity, notice: "Activity updated successfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to activities_path, notice: "Activity deleted successfully."
+  end
+
   private
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :category, :date, :duration)
+  end
+
   def set_activity
     @activity = Activity.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Activity not found."
+    redirect_to activities_path
   end
 end
