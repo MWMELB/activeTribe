0f85bcc145cc4e3bb3b6c2727c851ecd1ce655fe
Ivy Ta <131229797+ivy-ta@users.noreply.github.com>
2024-11-20 22:53:45 +1100

    all activity seeds match pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,33 +22,34 @@ puts "Creating 5 activities..."
 users = User.all
 melb_locations = ["Carlton, Victoria", "Southbank, Victoria", "Melbourne, Victoria", "Docklands, Victoria", "Fitzroy, Victoria"]
 activity_capacity = [10, 20, 30, 50, 100]
-activities = ["pilates", "running", "swimming", "surfing", "yoga"]
-puts "Creating activity 1"
+activities = ["pilates", "running", "pickleball", "surfing", "basketball", "hike"]
 
-
-activity_1_owner = users.sample
-activity_1_location = melb_locations.sample
-file_path_1 = Rails.root.join("app/assets/images/#{activities[0]}.jpg")
-file_1 = File.open(file_path_1)
-pilates = Activity.create(
-  user: activity_1_owner,
-  # Can change the title/description
-  title: "#{activities[0].capitalize} with #{activity_1_owner.first_name}",
-  category: activities[0],
-  description: "Join us in #{activity_1_location} for #{activities[0]}",
-  location: activity_1_location,
-  # Start time this upcoming week
-  # Faker timezone is UTC, so evening is equivalent to morning in Melbourne
-  start: (Faker::Time.between_dates(from: Date.today + 1, to: Date.today + 7, period: :evening)).beginning_of_hour,
-  # Duration range from 15min to 2hrs
-  duration: rand(1..8) * 15,
-  # Price range from free to $100
-  price: rand(0..20) * 5,
-  # Capacity range from 10 to unlimited
-  capacity: activity_capacity.sample
-)
-pilates.photo.attach(io: file_1, filename: "#{activities[0]}.jpg", content_type: "image/png")
-pilates.save
-file_1.close
+activities.each_with_index do |activity, index|
+  puts "Creating activity #{index + 1}"
+  owner = users.sample
+  location = melb_locations.sample
+  file_path = Rails.root.join("app/assets/images/#{activity}.jpg")
+  file = File.open(file_path)
+  new_activity = Activity.create(
+    user: owner,
+    # Can change the title/description
+    title: "#{activity.capitalize} with #{owner.first_name}",
+    category: activity,
+    description: "Join us in #{location} for #{activity}",
+    location: location,
+    # Start time this upcoming week
+    # Faker timezone is UTC, so evening is equivalent to morning in Melbourne
+    start: (Faker::Time.between_dates(from: Date.today + 1, to: Date.today + 7, period: :evening)).beginning_of_hour,
+    # Duration range from 15min to 2hrs
+    duration: rand(1..8) * 15,
+    # Price range from free to $100
+    price: rand(0..20) * 5,
+    # Capacity range from 10 to unlimited
+    capacity: activity_capacity.sample
+  )
+  new_activity.photo.attach(io: file, filename: "#{activity}.jpg", content_type: "image/png")
+  new_activity.save
+  file.close
+end
 
 puts "DONE!"
