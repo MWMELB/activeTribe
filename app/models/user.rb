76b72dd14f4ec58b09f0f3ac362d7ceae131559c
Moyu Watanabe <koteko9222w@gmail.com>
2024-11-21 20:30:34 +1100

    sign up page updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,18 @@ class User < ApplicationRecord
   has_many :activity_comments, dependent: :destroy
   has_many :groups, through: :group_users
   has_many :activities, through: :bookings
+
+  has_many :booked_activities, through: :bookings, source: :activity
   validates :first_name, :last_name, :birth_date, :email, presence: true
 
-  # # Attribute for terms agreement (sign-up page)
-  # attr_accessor :terms
+  # Attribute for terms agreement (sign-up page)
+  attr_accessor :terms
 
-  # validate :terms_accepted
+  validate :terms_accepted
 
-  # private
+  private
 
-  # def terms_accepted
-  #   errors.add(:terms, "must be accepted") unless terms == "1"
-  # end
-
-  # has_many :booked_activities, through: :bookings, source: :activity
-  # validates :first_name, :last_name, :birth_date, presence: true
-
+  def terms_accepted
+    errors.add(:terms, "must be accepted") unless terms == "1"
+  end
 end
