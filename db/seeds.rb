735fa5e85394e9f4903da8e90a64bf428e35735d
Ninna Romasanta <n.romasanta@outlook.com>
2024-11-23 10:58:35 +1100

    seed issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,21 +22,19 @@ puts "Creating 5 activities..."
 users = User.all
 melb_locations = ["Carlton, Victoria", "Southbank, Victoria", "Melbourne, Victoria", "Docklands, Victoria", "Fitzroy, Victoria"]
 activity_capacity = [10, 20, 30, 50, 100]
-activities = ["pilates", "running", "pickleball", "surfing", "basketball", "hike"]
 
-activities.each_with_index do |activity, index|
-  puts "Creating activity #{index + 1}"
-  owner = users.sample
-  location = melb_locations.sample
-  file_path = Rails.root.join("app/assets/images/#{activity}.jpg")
-  file = File.open(file_path)
-  new_activity = Activity.create(
-    user: owner,
+5.times do
+  activity_owner = users.sample
+  activity_category = Faker::Sport.sport(include_unusual: true)
+  activity_location = melb_locations.sample
+  activity_type = "biking"
+  Activity.create(
+    user: activity_owner,
     # Can change the title/description
-    title: "#{activity.capitalize} with #{owner.first_name}",
-    category: activity,
-    description: "Join us in #{location} for #{activity}",
-    location: location,
+    title: "#{activity_type} with #{activity_owner.first_name}",
+    category: activity_category,
+    description: "Join us in #{activity_location} for #{activity_type}",
+    location: activity_location,
     # Start time this upcoming week
     # Faker timezone is UTC, so evening is equivalent to morning in Melbourne
     start: (Faker::Time.between_dates(from: Date.today + 1, to: Date.today + 7, period: :evening)).beginning_of_hour,
@@ -47,9 +45,6 @@ activities.each_with_index do |activity, index|
     # Capacity range from 10 to unlimited
     capacity: activity_capacity.sample
   )
-  new_activity.photo.attach(io: file, filename: "#{activity}.jpg", content_type: "image/png")
-  new_activity.save
-  file.close
 end
 
 puts "DONE!"
